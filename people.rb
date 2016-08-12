ead4a9ed3235081084bf5fce013944fec4f0d9d3
Ryan <ryan.y.chung@gmail.com>
2016-08-12 14:27:40 -0400

    exercise1 created new file with 1 parent class and 2 child classes

---
+++ people.rb
@@ -0,0 +1,17 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Student < Person
+  def learn
+    puts "I get it!"
+  end
+end
+
+class Instructor < Person
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
+end
